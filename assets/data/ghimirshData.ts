f3d88104528caef49f3d592f0819757ac7600663
Shankar Ghimire <34399132+shankarghimire@users.noreply.github.com>
2021-07-28 11:09:37 -0400

    Add files via upload

---
+++ assets/data/ghimirshData.ts
@@ -0,0 +1,9 @@
+import {ghimirsh} from '../../app/ghimirsh';
+
+export const STUDENTDATA:ghimirsh={
+    studentNumber: 991585190,
+    name: 'Shankar Ghimire',
+    loginName: 'ghimirsh',
+    campusName: 'Davis Campus',
+    assignmentTitle: 'Assignment#03'
+}
---
+++ assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import {Book} from '../../app/bookInfo';
+
+export const BOOKSDATA : Book[]=[
+    {
+        bookName: 'Beginning Angular with TypeScript',
+        author: 'Greg Lim',
+        genre: 'Web Development/Frontend',
+        year:2020,
+        picture: 'assets/images/booksImages/angular-book.jpg'
+    },
+    {
+        bookName:'HeadFirst JavaScript',
+        author: 'Michael Morrison',
+        genre: 'Web Development/Frontend',
+        year: 2020,
+        picture: 'assets/images/booksImages/javascript-book.jpg'
+    },
+    {
+        bookName: 'Pro ASP.NET MVC 5',
+        author: 'Adam Freeman',
+        genre: 'Web Development/Backend',
+        year: 2021,
+        picture:'assets/images/booksImages/asp-book.jpg'
+    },
+    {
+        bookName: 'Promised Land',
+        author: 'Barak Obama',
+        genre: 'Auto-Biography',
+        year : 2020,
+        picture: 'assets/images/booksImages/promised-land-book.jpg'
+    }
+];
